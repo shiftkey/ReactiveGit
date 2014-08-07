f55d52a8f3033b2f016d1a54f5412eaf34e8d9b9
Brendan Forster <brendan@github.com>
2014-08-07 19:45:25 +1000

    a simple fetch + pull test done

---
+++ ReactiveGit.Tests/ObservableRepositoryTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+using Xunit;
+
+namespace ReactiveGit.Tests
+{
+    public class ObservableRepositoryTests
+    {
+        [Fact]
+        public async Task CanPullSomeRepository()
+        {
+            Credentials credentials = new UsernamePasswordCredentials
+            {
+                Username = "shiftkey-tester",
+                Password = "haha-password"
+            }; 
+
+            var repository = new ObservableRepository(
+                @"C:\Users\brendanforster\Documents\GìtHūb\atom-dark-syntax",
+                credentials);
+
+            var pullObserver = Observer.Create<Tuple<string, int>>(
+                next => { Console.WriteLine("progress: " + next.Item2); },
+                () => { Console.WriteLine("it is done"); })
+                .NotifyOn(DefaultScheduler.Instance);
+
+            var result = await repository.Pull(pullObserver);
+
+            Assert.NotEqual(MergeStatus.Conflicts, result.Status);
+        }
+    }
+}
---
+++ ReactiveGit/ObservableRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reactive.Linq;
+using LibGit2Sharp;
+
+namespace ReactiveGit
+{
+    public class ObservableRepository : IDisposable
+    {
+        readonly Repository _repository;
+        readonly Credentials _credentials;
+
+        public ObservableRepository(string directory)
+            : this(directory, null) { }
+
+        public ObservableRepository(string directory, Credentials credentials)
+        {
+            _repository = new Repository(directory);
+            _credentials = credentials;
+        }
+
+        public IObservable<MergeResult> Pull(
+            IObserver<Tuple<string, int>> observer)
+        {
+            var signature = _repository.Config.BuildSignature(DateTimeOffset.Now);
+            var options = new PullOptions
+            {
+                FetchOptions = new FetchOptions
+                {
+                    TagFetchMode = TagFetchMode.None,
+                    Credentials = _credentials,
+                    OnTransferProgress = progress =>
+                    {
+                        var p = (50 * progress.ReceivedObjects) / progress.TotalObjects;
+                        observer.OnNext(Tuple.Create("", p));
+                        return true;
+                    }
+                },
+                MergeOptions = new MergeOptions
+                {
+                    OnCheckoutProgress = (s, completedSteps, totalSteps) =>
+                    {
+                        // surface the progress from libgit2
+                        var progress = 50 + (50 * completedSteps) / totalSteps;
+                        observer.OnNext(Tuple.Create(s, progress));
+                    }
+                }
+            };
+
+            // this is a blocking call, hence all the ceremony before
+            return Observable.Start(() => _repository.Network.Pull(signature, options))
+                .Finally(() =>
+                {
+                    // ensure the observable signals even when the branch is up to date
+                    observer.OnNext(Tuple.Create("pull completed", 100));
+                    observer.OnCompleted();
+                });
+        }
+
+        public void Dispose()
+        {
+            _repository.Dispose();
+        }
+    }
+}
