82a9eca0a60a6c68f448dc3d7dfe18429592e03a
Brendan Forster <brendan@github.com>
2014-08-07 20:00:20 +1000

    add a test for pushing and progress bar

--- ReactiveGit.Tests/ObservableRepositoryTests.cs
+++ ReactiveGit.Tests/ObservableRepositoryTests.cs
@@ -17,20 +17,40 @@ namespace ReactiveGit.Tests
             {
                 Username = "shiftkey-tester",
                 Password = "haha-password"
-            }; 
+            };
 
             var repository = new ObservableRepository(
                 @"C:\Users\brendanforster\Documents\GìtHūb\atom-dark-syntax",
                 credentials);
 
+            var progress = 0;
+
             var pullObserver = Observer.Create<Tuple<string, int>>(
-                next => { Console.WriteLine("progress: " + next.Item2); },
-                () => { Console.WriteLine("it is done"); })
+                next =>
+                {
+                    Console.WriteLine("pull progress: " + next.Item2);
+                    progress = (next.Item2 * 2) / 3;
+                },
+                () => { Console.WriteLine("pull completed"); });
+
+            var pullResult = await repository.Pull(pullObserver);
+
+            Assert.Equal(66, progress);
+
+            Assert.NotEqual(MergeStatus.Conflicts, pullResult.Status);
+
+            var pushObserver = Observer.Create<Tuple<string, int>>(
+                next =>
+                {
+                    progress = 66 + (next.Item2 * 2) / 3;
+                    Console.WriteLine("push progress: " + next.Item2);
+                },
+                () => { Console.WriteLine("push completed"); })
                 .NotifyOn(DefaultScheduler.Instance);
 
-            var result = await repository.Pull(pullObserver);
+            var pushResult = await repository.Push(pushObserver);
 
-            Assert.NotEqual(MergeStatus.Conflicts, result.Status);
+            Assert.Equal(100, progress);
         }
     }
 }
