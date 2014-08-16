ecae67eba683e7f688a16f78016d905da1ab49f8
Brendan Forster <brendan@github.com>
2014-08-16 16:53:25 +1000

    wireup creation of the nuget packages

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 # assumes Powershell 3+ due to $PSScriptRoot
+$root = $PSScriptRoot
 
 if (Test-Path  $env:ProgramFiles\MSBuild\12.0\bin\) {
   $msbuild = "$env:ProgramFiles\MSBuild\12.0\bin\msbuild.exe"
@@ -6,4 +7,16 @@ if (Test-Path  $env:ProgramFiles\MSBuild\12.0\bin\) {
   # TODO: fail this script
 }
 
-. $msbuild "$PSScriptRoot\ReactiveGit.sln" /target:Rebuild /property:Configuration=Release
+# ensure you're running in the script root
+Push-Location -Path $root
+
+# build the thing
+. $msbuild .\ReactiveGit.sln /target:Rebuild /property:Configuration=Release
+
+# TODO: run the tests
+
+# create the packages
+.\tools\nuget.exe pack .\nuspec\ReactiveGit.nuspec
+.\tools\nuget.exe pack .\nuspec\ReactiveGit.Source.nuspec
+
+Pop-Location
