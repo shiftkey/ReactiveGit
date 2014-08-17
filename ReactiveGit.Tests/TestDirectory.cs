67d741879722d3b5c0aec582bd2038e5e3562eca
Brendan Forster <brendan@github.com>
2014-08-17 12:13:23 +1000

    added the clone operation and a simple test harness

---
+++ ReactiveGit.Tests/TestDirectory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace ReactiveGit.Tests
+{
+    public class TestDirectory : IDisposable
+    {
+        public TestDirectory()
+        {
+            Path = System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetRandomFileName());
+        }
+
+        public string Path { get; private set; }
+
+        public static TestDirectory Create()
+        {
+            return new TestDirectory();
+        }
+
+        public async void Dispose()
+        {
+            await Observable.Start(
+                () => Directory.Delete(Path, true))
+            .Retry(3);
+        }
+    }
+}
---
+++ ReactiveGit/ObservableRepository.Clone.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using LibGit2Sharp;
+
+namespace ReactiveGit
+{
+    public partial class ObservableRepository
+    {
+        public static IObservable<IObservableRepository> Clone(
+            string sourceUrl,
+            string workingDirectory,
+            IObserver<Tuple<string, int>> observer,
+            Credentials credentials = null)
+        {
+            var isCancelled = false;
+            var options = new CloneOptions
+            {
+                Checkout = true,
+                Credentials = credentials,
+                OnTransferProgress = progress =>
+                {
+                    var p = (100 * progress.ReceivedObjects) / progress.TotalObjects;
+                    observer.OnNext(Tuple.Create("", p));
+                    return !isCancelled;
+                },
+                IsBare = false,
+                OnCheckoutProgress = ProgressFactory.CreateHandler(observer)
+            };
+
+            return Observable.Create<ObservableRepository>(subj =>
+            {
+                var sub = Observable.Start(() =>
+                {
+                    var directory = Repository.Clone(sourceUrl, workingDirectory, options);
+
+                    observer.OnNext(Tuple.Create("clone completed", 100));
+                    observer.OnCompleted();
+
+                    return new ObservableRepository(directory, credentials);
+                }, Scheduler.Default).Subscribe(subj);
+
+                return new CompositeDisposable(
+                    sub,
+                    Disposable.Create(() =>
+                    {
+                        isCancelled = true;
+                        observer.OnCompleted();
+                    }));
+            });
+        }
+    }
+}
