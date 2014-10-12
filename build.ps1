1e0674d1dd004fb937e5010c09ebad32fb43b9a5
Brendan Forster <brendan@github.com>
2014-10-12 11:29:21 -0300

    move all of this into a FAKE script

--- build.ps1
+++ build.ps1
@@ -1,24 +1,15 @@
 # assumes Powershell 3+ due to $PSScriptRoot
 $root = $PSScriptRoot
-$version = "0.0.2"
-
-if (Test-Path  $env:ProgramFiles\MSBuild\12.0\bin\) {
-  $msbuild = "$env:ProgramFiles\MSBuild\12.0\bin\msbuild.exe"
-} else {
-  # TODO: fail this script
-}
 
 # ensure you're running in the script root
 Push-Location -Path $root
 
+# grab FAKE
+.\tools\nuget.exe install FAKE.Core -OutputDirectory tools -ExcludeVersion -version 3.5.8
+
 # build the thing
 .\tools\nuget.exe restore .\ReactiveGit.sln
-. $msbuild .\ReactiveGit.sln /target:Rebuild /property:Configuration=Release
 
-# TODO: run the tests
-
-# create the packages
-.\tools\nuget.exe pack .\nuspec\ReactiveGit.nuspec -Version $version
-.\tools\nuget.exe pack .\nuspec\ReactiveGit.Source.nuspec -Version $version
+.\tools\FAKE.Core\tools\Fake.exe build.fsx target=Package
 
 Pop-Location
