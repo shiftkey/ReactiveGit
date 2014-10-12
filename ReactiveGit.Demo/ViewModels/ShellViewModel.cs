e83dc142fe637410edc4f35120446355ef46610a
Brendan Forster <brendan@github.com>
2014-10-12 13:05:58 -0300

    getting some project structure right

--- ReactiveGit.Demo/ViewModels/ShellViewModel.cs
+++ ReactiveGit.Demo/ViewModels/ShellViewModel.cs
@@ -1,10 +1,19 @@
-﻿using ReactiveUI;
+﻿using System.IO;
+using System.Reactive.Linq;
+using ReactiveUI;
 
 namespace ReactiveGit.Demo.ViewModels
 {
     public class ShellViewModel : ReactiveObject
     {
+        public ShellViewModel()
+        {
+            // TODO: user can open a folder on disk
+            
+            // TODO: if that folder is valid (and is a repository)
+            // TODO: we should display options to checkout, push and pull
 
+        }
 
 
 
---
+++ ReactiveGit.Demo/Views/ShellView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using ReactiveGit.Demo.ViewModels;
+using ReactiveUI;
+
+namespace ReactiveGit.Demo.Views
+{
+    public partial class ShellView : IViewFor<ShellViewModel>
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+            "ViewModel", typeof (ShellViewModel), typeof (ShellView), new PropertyMetadata(default(ShellViewModel)));
+
+        public ShellViewModel ViewModel
+        {
+            get { return (ShellViewModel) GetValue(ViewModelProperty); }
+            set { SetValue(ViewModelProperty, value); }
+        }
+
+        object IViewFor.ViewModel
+        {
+            get { return ViewModel; }
+            set { ViewModel = value as ShellViewModel; }
+        }
+    }
+}
