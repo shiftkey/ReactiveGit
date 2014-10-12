90ee979867f2c7d0b0ce6a839ab24c5ead53ad44
Brendan Forster <brendan@github.com>
2014-10-12 12:58:01 -0300

    started a new project yaaaay!

---
+++ ReactiveGit.Demo/ViewModels/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+
+namespace ReactiveGit.Demo.ViewModels
+{
+    public class ShellViewModel : ReactiveObject
+    {
+
+
+
+
+
+        string _repositoryPath;
+        public string RepositoryPath
+        {
+            get { return _repositoryPath; }
+            set { this.RaiseAndSetIfChanged(ref _repositoryPath, value); }
+        }
+    }
+}
